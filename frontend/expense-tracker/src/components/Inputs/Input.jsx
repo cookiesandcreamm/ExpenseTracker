53537f92a5346c7223dbd36127c820ccf0ac0c87
cookiesandcreamm <78111330+cookiesandcreamm@users.noreply.github.com>
2025-08-05 16:26:33 -0500

    Addding all the login features

--- frontend/expense-tracker/src/components/Inputs/Input.jsx
+++ frontend/expense-tracker/src/components/Inputs/Input.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useState } from "react";
+{/* Importing icons used to show/hide passwords, Eye - when password is visible, Slash - when password is hidden*/}
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6"
 
 const Input = ({ value, onChange, label, placeholder, type }) => {
+    {/* Local state that tracks if password should be visible, default is false (hidden) */}
     const [showPassword, setShowPassword] = useState(false);
 
+    {/* Function toggles between true and false when the eye icon is clicked */}
     const toggleShowPassword = () => {
             setShowPassword(!showPassword);
         };
@@ -12,15 +15,18 @@ const Input = ({ value, onChange, label, placeholder, type }) => {
         <div>
             <label className="text-[13px] text-slate-800">{label}</label>
 
+            {/* The box where people type in input, styling for it is in index.css */}
             <div className="input-box">
+                {/* Styling for the space the input is being typed into */}
                 <input
                     type= {type == 'password' ? showPassword ? 'text' : 'password' : type}
                     placeholder={placeholder}
-                    className="w-full bg-transparent outline-none"
+                    className="w-full bg-transparent outline-none" 
                     value={value}
                     onChange={(e) => onChange(e)}
                 />
 
+                {/* If else condition for the eye icon */}
                 {type === "password" && (
                     <>
                         {showPassword ? (
--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import { useNavigate } from 'react-router-dom';
-import Input from "../../components/Inputs/Input"
+import { Link, useNavigate } from 'react-router-dom';
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -12,7 +13,24 @@ const Login = () => {
     const navigate = useNavigate();
 
     // Handle Login Form Submit
-    const handleLogin = async (e) => {}
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!password) {
+            setError("Please enter the password");
+            return;
+        }
+
+        setError("");
+
+        //Login API Call
+
+    }
 
     return (
         <AuthLayout>
@@ -30,6 +48,28 @@ const Login = () => {
                         placeholder="john@example.com"
                         type="text"
                     />
+
+                    <Input
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                        label="Password"
+                        placeholder="Min 8 Characters"
+                        type="password"
+                    />
+
+                    {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+                    {/* Styling for the login button is in index.css */}
+                    <button type="submit" className="btn-primary">
+                        LOGIN
+                    </button>
+
+                    <p className="text-[13px] text-slate-800 mt-3">
+                        Don't have an account?{" "}
+                        <Link className="font-medium text-primary underline" to="/signup">
+                            Sign Up
+                        </Link>
+                    </p>
                 </form>
             </div>
         </AuthLayout>
