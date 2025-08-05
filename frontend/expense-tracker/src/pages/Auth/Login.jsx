3c0ad5f5a6b61ecc3b068e37ddc314241f3a5c74
cookiesandcreamm <78111330+cookiesandcreamm@users.noreply.github.com>
2025-08-05 16:49:47 -0500

    Adding comment for login

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -34,6 +34,7 @@ const Login = () => {
 
     return (
         <AuthLayout>
+            {/* The children prop that goes into the AuthLayout component */}
             <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
                 <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
                 <p className="text-xs text-slate-700 mt-[5px] mb-6">
